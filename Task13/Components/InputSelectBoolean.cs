8ece60f9deed81730d69005bcfe5d7b6c2150573
weavelol <deniss.shutov98@gmail.com>
2022-02-01 01:13:58 +0200

    CheckBox -> InputSelectBoolean

---
+++ Task13/Components/InputSelectBoolean.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Task13.Components {
+    public class InputSelectBoolean : InputSelect<bool> {
+        protected override bool TryParseValueFromString(string value, out bool result,
+            out string validationErrorMessage) {
+            if (bool.TryParse(value, out var resultBool)) {
+                result = resultBool;
+                validationErrorMessage = string.Empty;
+                return true;
+            }
+
+            result = default!;
+            validationErrorMessage = "The chosen value is not a valid boolean.";
+            return false;
+        }
+    }
+}
