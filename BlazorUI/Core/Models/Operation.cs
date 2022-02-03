74e91f8d309018f9ba38b51a08cf821cb8fd7588
weavelol <deniss.shutov98@gmail.com>
2022-02-03 02:45:32 +0200

    fixed naming for serialization

--- BlazorUI/Core/Models/Operation.cs
+++ BlazorUI/Core/Models/Operation.cs
@@ -2,7 +2,7 @@
 
 namespace Core.Models {
     public class Operation : AbstractModel {
-        public string OperationTypeName { get; set; }
+        public OperationType OperationTypeDTO { get; set; }
         public DateTime Date { get; set; }
         public int Amount { get; set; }
 
